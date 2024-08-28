ede7b95c12e8e32cf08cd54defbabe44a529b02e
Thông Nguyễn <nguyenduongtienthong@gmail.com>
2024-08-28 18:23:28 +0700

    🚀 Finish 💫 deploy testing ☁️

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-const express = require('express');;
+const express = require('express');
 
 const app = express();
 const port = 3000;
 
 app.use(express.static('build'));
 app.get('/', (req, res) => {
+  res.send("hello")
 });
 
 // Your API route
